74582eb60c6f334502a36ce4c694f3a217bf0758
Mahua <majid.sunny@technonext.com>
2025-09-06 16:27:25 +0600

    Add -- success and error code

---
+++ src/main/java/com/example/dicegame/constant/PlayerStatusDictionary.java
@@ -0,0 +1,24 @@
+package com.example.dicegame.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Player-related status codes and messages.
+ */
+@Getter
+@RequiredArgsConstructor
+public enum PlayerStatusDictionary {
+
+    //Success code & messages
+    PLAYER_FETCH_SUCCESS("901-004-0001-0", "Player fetched successfully"),
+    PLAYER_SAVED_SUCCESS("901-004-0002-0", "Player saved successfully"),
+    PLAYER_PAGE_FETCH_SUCCESS("901-004-0003-0", "Player fetched successfully with pagination"),
+
+    // Error code & messages
+
+    SUBSCRIPTION_PLAN_FETCH_FAILED("901-004-2001-1", "Failed to fetch player");
+
+    private final String statusCode;
+    private final String message;
+}
