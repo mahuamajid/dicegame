e96829dbe075d2e16a06ddc0fb0570da23fef212
Mahua <majid.sunny@technonext.com>
2025-09-06 19:34:46 +0600

    Add -- feign client config

--- src/main/java/com/example/dicegame/DicegameApplication.java
+++ src/main/java/com/example/dicegame/DicegameApplication.java
@@ -2,10 +2,10 @@ package com.example.dicegame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.example.dicegame.model.entity")
+@EnableFeignClients
 public class DicegameApplication {
 
 	public static void main(String[] args) {
