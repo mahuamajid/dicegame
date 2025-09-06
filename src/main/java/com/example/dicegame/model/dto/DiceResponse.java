e19f4c17a344cb4737e7df99e6cf69d9a9abea45
Mahua <majid.sunny@technonext.com>
2025-09-06 15:13:26 +0600

    Add -- dto DiceResponse

---
+++ src/main/java/com/example/dicegame/model/dto/DiceResponse.java
@@ -0,0 +1,12 @@
+package com.example.dicegame.model.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DiceResponse {
+    private int value;
+}
