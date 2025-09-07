a3ccbd3cd6081d140bf131339034a349b77e33d6
Mahua <majid.sunny@technonext.com>
2025-09-07 16:36:51 +0600

    Fix: -- roll-dice api call

--- src/main/java/com/example/dicegame/client/support/RollDiceSupport.java
+++ src/main/java/com/example/dicegame/client/support/RollDiceSupport.java
@@ -1,13 +1,15 @@
 package com.example.dicegame.client.support;
 
 import com.example.dicegame.client.RollDiceClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 
 @Component
+@RequiredArgsConstructor
 public class RollDiceSupport {
-    private RollDiceClient rollDiceClient;
+    private final RollDiceClient rollDiceClient;
 
     public int roll() {
        return Objects.requireNonNull(rollDiceClient.roll().getBody()).getValue();
